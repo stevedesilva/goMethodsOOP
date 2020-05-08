02c51b58e9a4fd88285ab829479abcd6f020daab
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-05-08 10:07:29 +0100

    refactor

--- bookgame/book.go
+++ bookgame/book.go
@@ -3,22 +3,49 @@ package bookgame
 import (
 	"fmt"
 	m "goMethodsOOP/money"
+	"strconv"
+	"time"
 )
 
 // receiver name should be consistent with previous receiver name b for Book
 
 // Book struct
 type Book struct {
-	Title string
-	Price m.Money
+	Title     string
+	Price     m.Money
+	Published interface{}
 }
 
 // Print func
 func (b *Book) Print() {
-	fmt.Printf("%-15s: %s\n", b.Title, b.Price.ToString())
+	p := format(b.Published)
+	fmt.Printf("%-15s: %s - (%v)\n", b.Title, b.Price.ToString(), p)
 }
 
 // Discount price
 func (b *Book) Discount(ratio float64) {
 	b.Price *= m.Money(1 - ratio)
+}
+
+func format(x interface{}) string {
+	// &bookgame.Book{Title: "Moby Dick", Price: 20},
+	if x == nil {
+		return "unknown"
+
+	}
+	// &bookgame.Book{Title: "Astrix", Price: 40, Published: 234244979},
+	var t int
+
+	if i, ok := x.(int); ok {
+		t = i
+	}
+
+	// &bookgame.Book{Title: "Tintin", Price: 40, Published: "123456959"},
+	if i, ok := x.(string); ok {
+		t, _ = strconv.Atoi(i)
+	}
+
+	u := time.Unix(int64(t), 0)
+	return u.String()
+
 }
--- cmd/game/main.go
+++ cmd/game/main.go
@@ -1,39 +1,34 @@
 package main
 
-import (
-	"goMethodsOOP/bookgame"
-	"goMethodsOOP/list"
-)
-
 // https://circleci.com/gh/stevedesilva/workflows/goMethodsOOP
 func main() {
 
-	doom := bookgame.Game{Title: "Doom", Price: 40}
-	gta := bookgame.Game{Title: "gta", Price: 60}
+	// doom := bookgame.Game{Title: "Doom", Price: 40}
+	// gta := bookgame.Game{Title: "gta", Price: 60}
 
 	// - create array game pointer
 
 	// Alternative
 	// items := []*g.Game(nil)
-	// or	
+	// or
 	// var items []*g.Game
 
 	// // - append to item
 	// items = append(items, &doom, &gta)
 
-	items := []*bookgame.Game{&doom, &gta}
+	// items := []*bookgame.Game{&doom, &gta}
 
 	// Cast to List (i.e.type List []*g.Game)
 	// Note: You can convert types that have identical underlying type
 	// List type and items share same underlying type []*g.Game
 	// casting to List decorates items with List methods (e.g. Listprint)
 	// could use new
-	my := list.List(items)
+	// my := list.List(items)
 
 	// you can attach methods to a nil valiue
 	// my = nil
 
 	// l.Listprint(items)
-	// 
-	my.Listprint()
+	//
+	// my.Listprint()
 }
