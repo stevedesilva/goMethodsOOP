5231fbbdad7ca06b4574538affdbb7c84a667b90
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-05-08 09:01:28 +0100

    add more data

--- cmd/empty/main.go
+++ cmd/empty/main.go
@@ -3,7 +3,44 @@ package main
 import "fmt"
 
 func main() {
+	// emptyOne()
+
+	emptyTwo()
+
+	// var many []interface{}
+	// many = nums
+}
+
+func emptyTwo() {
+	nums := []int{1, 2, 3}
 	var any interface{}
+	// CAN assign anything to interface{} type
+	any = nums
+	fmt.Printf("1 Dynamic type %T \tDynamic value %[1]v\n", any)
+
+	// CANNOT assign interface{} to []int type
+	//nums = any
+
+	// you cannot use any as an int slice. any is an empty interface value , it isn't an int slice
+	// _ = len(any)
+	_ = len(any.([]int))
+
+	var many []interface{}
+	// many = nums
+
+	// 
+	for _, v := range nums {
+		many = append(many, v)
+	}
+	fmt.Println(many)
+
+}
+
+func emptyOne() {
+	var any interface{}
+	// Dynamic type <nil>    Dynamic value <nil>
+	fmt.Printf("1 Dynamic type %T \tDynamic value %[1]v\n", any)
+
 	// you can store any value
 	any = []int{1, 2, 3}
 	any = map[int]bool{1: true, 2: false}
@@ -11,11 +48,7 @@ func main() {
 	any = 1
 
 	// you cannot directly use the dynamic value of an empty interface value (2 * any)
-
+	// Dynamic type []int    Dynamic value [1 2 3]
 	any = 2 * any.(int)
 	fmt.Println(any)
-
-	nums := []int{1, 2, 3}
-	any = nums
-	fmt.Printf("Dynamic type %T \tDynamic value %[1]v\n",any)
 }
