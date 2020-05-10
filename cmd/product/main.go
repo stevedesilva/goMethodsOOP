fbab977038e718929c42516563685ca5d7fab548
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-05-10 12:48:25 +0100

    fix

--- cmd/product/main.go
+++ cmd/product/main.go
@@ -3,19 +3,19 @@ package main
 // "fmt"
 import (
 	"fmt"
-	// p "goMethodsOOP/product"
+	p "goMethodsOOP/product"
 	m "goMethodsOOP/money"
 )
 
 func main() {
-	// l := p.List{
-	// 	&p.Product{Title: "Moby Dick", Price: 20},
-	// 	&p.Product{Title: "Astrix", Price: 25, Released: p.ToTimestamp(234244979)},
-	// 	&p.Product{Title: "Tintin", Price: 40, Released: p.ToTimestamp("123456959")},
-	// }
-	// l.Discount(.5)
-	// fmt.Print(l)
-		var pocket m.Money = 10
-		fmt.Println("I have ",pocket)
+	l := p.List{
+		&p.Product{Title: "Moby Dick", Price: 20},
+		&p.Product{Title: "Astrix", Price: 25, Released: p.ToTimestamp(234244979)},
+		&p.Product{Title: "Tintin", Price: 40, Released: p.ToTimestamp("123456959")},
+	}
+	l.Discount(.5)
+	fmt.Print(l)
+	var pocket m.Money = 10
+	fmt.Println("I have ", pocket)
 
 }
