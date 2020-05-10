54e4c9fb61b3b6c388573bac54e66ae69e5751f7
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-05-10 11:58:24 +0100

    stringer

--- product/list.go
+++ product/list.go
@@ -15,7 +15,6 @@ func (l List) Print() {
 		return
 	}
 	// - print
-	fmt.Println("Print ....")
 	for _, v := range l {
 		v.Print()
 	}
@@ -23,10 +22,26 @@ func (l List) Print() {
 
 // Discount func
 func (l List) Discount(discount float64) {
-	fmt.Printf("Discounting by %v  \n.", discount)
+	fmt.Printf("Discounting by %v  \n", discount)
 
 	// "it" here is Item
 	for _, p := range l {
 		p.Discount(discount)
 	}
+}
+
+// Len is the number of elements in the collection.
+func (l List) Len() int {
+	return len(l)
+}
+
+// Less reports whether the element with
+// index i should sort before the element with index j.
+func (l List) Less(i, j int) bool {
+	return l[i].Title < l[j].Title
+}
+
+// Swap swaps the elements with indexes i and j.
+func (l List) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
 }
